fb01694bde4edd28a3b03ca27582fb941d31a007
Monnet joris <joris.monnet@he-arc.ch>
2020-02-24 13:49:07 +0100

    switch branchs

--- AppWeb/app/views/about.view.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $title = "About " . $company;
-    require('partials/header.php')
-?>
-<div class="main">
-  <h1>About <?= htmlentities($company) ?></h1>
-</div>
-<?php require('partials/footer.php') ?>
