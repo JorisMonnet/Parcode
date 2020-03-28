8394a3362ba72c8d5075783267d423f39a6baf4f
Monnet Joris <joris.monnet@he-arc.ch>
2020-03-29 00:14:43 +0100

    remove unnecessary pages

--- app/views/home.view.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $title = "Home " . $company;
-    require('partials/header.php')
-?>
-<div class="main">
-  <h1>Home <?= htmlentities($company) ?></h1>
-</div>
-<?php require('partials/footer.php') ?>
