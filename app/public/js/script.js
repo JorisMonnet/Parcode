bf876c0faf0893907133d0eb503b337390d48d26
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-03-30 16:10:24 +0200

    cleaning my mistakes

--- app/public/js/script.js
+++
@@ -1,7 +0,0 @@
-function showForm(){
-    if(document.getElementById('hiddenForm').style.visibility === 'hidden'){
-        document.getElementById('hiddenForm').style.visibility = 'visible';
-    }else{
-        document.getElementById('hiddenForm').style.visibility = 'hidden';
-    }
-}
