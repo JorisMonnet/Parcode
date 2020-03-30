ef39fc7b7fd10329e81f61256f5aa988b879e255
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-03-30 15:53:33 +0200

    adding css to login page

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -3,17 +3,23 @@
     require('partials/header.php');
 ?>
 <form class="log" action="login" method="post">
-  <h1>Login</h1>
+  <h1 class="login_elem" >
+    <div id="logo_parcode">PARCODE</div>
+    <br/>Login</h1>
   <label for="username">Username:</label>
-  <input type="text" name="user" id="username"><br>
+  <input type="text" name="user" id="username" class="input_text"><br>
   <label for="password">Password:</label>
-  <input type="password" name="pass" id="password"><br>
-  <input type="submit" value="submit">
+  <input type="password" name="pass" id="password" class="input_text"><br>
+  <input type="submit" class="button" value="submit">
+  <p class="login_elem">
+    Fresh with us ?<br>
+    
+  </p>
 </form>
 <?php if(Logger::lastLogEventisFalseLog()){
   echo '<div id = "falseLog">';
-    echo '<br> user not found <br>';
+    echo 'user not found <br>';
     echo 'please try again <br>';
   echo '</div>';
-} ?>
+  } ?>
 <?php require('partials/footer.php'); ?>
