5ea720b1d00b3c64a22ff4f23ab44ad925e4129c
Monnet Joris <joris.monnet@he-arc.ch>
2020-04-20 14:15:02 +0200

    codecommentModel is used by coe and comments classe, it allows us to don't repeat the common codes between them and simplify

--- app/models/CodeCommentModel.php
+++ app/models/CodeCommentModel.php
@@ -2,33 +2,58 @@
 require_once('User.php');
 abstract class CodeCommentModel extends Model
 {
+    protected $content;
+	protected $date;
+    protected $author;
+
+    public function getContent(){
+		return $this->content;
+	}
+
+  	public function setContent($value){
+        $this->content = $value;
+    }
+
+  	public function getDate(){
+		return $this->date;
+	}
+
+  	public function setDate($value){
+        $this->date = $value;
+    }
+
+	public function getAuthor(){
+		return $this->author;
+	}
+	
+	public function setAuthor($value){
+		$this->author = $value;
+	}
     public function asHTMLTableRow(){
         return $this->strWithoutAuthor().$this->strAuthor();
     }
-    public static function fetchAllComments($sort,$order,$id){
-        $dbh = App::get('dbh');
-        $statement = $dbh->prepare("select * from ".get_called_class()." WHERE codes= ? ORDER BY ".$sort." ".$order);
-        $statement->bindParam(1,$id,PDO::PARAM_INT);
-        $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_CLASS, get_called_class());
-    }
-    public function asHTMLTableRowWithEdit($user){
-        $str = $this->strWithoutAuthor();
-        if($this->author===$user)
-            $str .= '</div><button id="buttonEditComment" type="button" onclick="showForm()">edit Code</button>';
-        else
-            $str.=$this->strAuthor();
-        return $str;
-    }
-    private function strWithoutAuthor(){
-        $str = "<div>";
-        //$str .= "<a href=\"code?id=".urlencode($this->id)."\">".htmlentities($this->id) ." </a><br><br>";
-        $str .= htmlentities($this->content)."<br><br>";
-        $str .= date("j F Y H:i:s",strtotime($this->date))."<br>";
-        return $str;
-    }
     private function strAuthor(){
-        $authorName = User::fetchSomething($this->author,"id");
+        $authorName = User::fetchSomething($this->getAuthor(),"id");
         return htmlentities($authorName->getName())."</div>";
     }
+    public function asHTMLTableRowWithEdit($user){
+		$str = $this->strWithoutAuthor();
+		if($this->getAuthor()===$user)
+            if(get_class($this)=="Comments"){
+                $str .= '<input type="hidden"  name="commentId" value="'.htmlentities($this->getId()).'"> ';
+                $str .= '</div><button class="button" type="button" onclick="showCommentForm()">edit Comment</button>';
+            } else
+                $str .= "<br><a href=\"codeUpdate?id=".urlencode($this->getId())."\"> Edit Code </a>";
+		else
+			$str.=$this->strAuthor();
+        return $str;
+	}
+	private function strWithoutAuthor(){
+        $str = "<div>";
+        if(get_class($this)=="Codes")
+		    $str .= "<a href=\"code?id=".urlencode($this->getId())."\">".htmlentities($this->getId()) ." </a><br><br>";
+		$str .= "<code><pre>".htmlentities($this->getContent())."</pre></code><br><br>";
+		$str .= date("j F Y H:i:s",strtotime($this->getDate()))."<br>";
+		return $str;
+	}
 }
--- app/models/Comments.php
+++ app/models/Comments.php
@@ -4,45 +4,19 @@ require_once('Codes.php');
 /**
 * The Comments class
 */
-class Comments extends Model
+class Comments extends CodeCommentModel
 {
-    // Attributes
-	private $content;
-	private $date;
-    private $author;
+    // Attribute
     private $codes;
 
-  // Getters and Setters
-
-	public function getContent(){
-		return $this->content;
-	}
-
-  	public function setContent($value){
-        $this->content = $value;
-    }
-
-  	public function getDate(){
-		return $this->date;
-	}
-
+  	// Getter and Setter
     public function getCodes(){
         return $this->codes;
     }
-  	public function setDate($value){
-        $this->date = $value;
-    }
-
-	public function getAuthor(){
-		return $this->author;
-	}
-
-	public function setAuthor($value){
-		$this->author = $value;
-	}
     public function setCodes($value){
         $this->codes = $value;
-    }
+	}
+
 	public static function getParam(){
 		return [
 			"content" => PDO::PARAM_STR,
@@ -61,10 +35,7 @@ class Comments extends Model
             'codes' => $this->getCodes()
 		];
 	}
-
-  	public function asHTMLTableRow(){
-      	return $this->strWithoutAuthor().$this->strAuthor();
-    }
+  	
 	public static function fetchAllComments($sort,$order,$id){
 		$dbh = App::get('dbh');
 		$statement = $dbh->prepare("select * from ".get_called_class()." WHERE codes= ? ORDER BY ".$sort." ".$order);
@@ -72,24 +43,5 @@ class Comments extends Model
 		$statement->execute();
 	return $statement->fetchAll(PDO::FETCH_CLASS, get_called_class());
 	}
-  	public function asHTMLTableRowWithEdit($user){
-		$str = $this->strWithoutAuthor();
-		if($this->author===$user){
-			$str .= '<input type="hidden"  name="commentId" value="'.htmlentities($this->getId()).'"> ';
-			$str .= '</div><button class="button" type="button" onclick="showCommentForm()">edit Comment</button>';
-		} else
-			$str.=$this->strAuthor();
-        return $str;
-	}
-	private function strWithoutAuthor(){
-		$str = "<div>";
-		//$str .= "<a href=\"code?id=".urlencode($this->id)."\">".htmlentities($this->id) ." </a><br><br>";
-		$str .= htmlentities($this->content)."<br><br>";
-		$str .= date("j F Y H:i:s",strtotime($this->date))."<br>";
-		return $str;
-	}
-	private function strAuthor(){
-		$authorName = User::fetchSomething($this->author,"id");
-		return htmlentities($authorName->getName())."</div>";
-	}
+  	
 }
