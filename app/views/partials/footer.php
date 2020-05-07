49489facdce4f7d2ec58a6cc03974e54b21c855e
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-07 15:25:01 +0200

    replace footer for pages Home/login/signUp, add background color cf Objective Interface Utilisateur

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,5 +1,7 @@
-    
-	<footer id='footer'>
+<?php if (!preg_match_all('/Code*/',$title))
+		echo '<footer style="position:absolute;bottom:0">';
+	else
+		echo '<footer>';?>
 		Projet Application Web SP 2020 - HE-ARC INF2-DLMB - JORIS MONNET, ADRIEN PAYSANT 
 	</footer>
     </body>
