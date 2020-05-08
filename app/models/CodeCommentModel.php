ec7d4041a952f82ce7119c73ecc30cbadb42300a
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-08 22:21:22 +0200

    disable the vote for not connected user cf Objectives : Comments - upvote

--- app/models/CodeCommentModel.php
+++ app/models/CodeCommentModel.php
@@ -37,7 +37,7 @@ abstract class CodeCommentModel extends Model
     private function strAuthor($i){
 		$authorName = User::fetchSomething($this->getAuthor(),"id");
 		$str = '<span> Authored by '.htmlentities($authorName->getName());
-		if(get_class($this)=="Comments"){
+		if(get_class($this)=="Comments"&& isset($_SESSION['userid'])){
 			$str.='<span class="idComment hiddenForm" hidden>'.htmlentities($this->getId()).'</span>';
 			$str.='<img class="glyphicon_up" src="app/views/partials/chevron_up.png" alt="upvote" onload="addVote('.$i.')" onclick="listVotes['.$i.'].upvote()"></img>
 					<span class="voteLabel">'.$this->getVotes().'</span>
