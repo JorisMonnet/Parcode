59bbe7c31e84cb348a8af73fabb25a38d67d98cc
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-12 20:26:46 +0200

    add an example for the groups for the user cf Objective User Interface

--- app/views/addCode.view.php
+++ app/views/addCode.view.php
@@ -11,7 +11,7 @@
         <h2>Code :</h2>
         <textarea name="content" class="bigTextarea" required></textarea>
         <h2>Groups(only characters and numbers) :</h2>
-        <p>You can add a group by putting a "." between the two groups</p>
+        <p>You can add a group by putting a "." between the two groups <br>(example: appWeb.php.projets)</p>
         <input type="text" name="groups" class="inputText" id="inputTextGroups" pattern="^([\w]+([.]?[\w]+)+)+$" required>
         <input type="submit" class="button" value="Submit">
     </form>
