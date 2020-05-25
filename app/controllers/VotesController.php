05d78ecefdb2689946ef0db8826d27835a2f0d69
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-25 15:25:31 +0200

    autodoc on VotesController

--- app/controllers/VotesController.php
+++ app/controllers/VotesController.php
@@ -2,8 +2,16 @@
 
 require_once('app/models/Votes.php');
 
+/**
+ * Class which controlls the votes by the users
+ */
 class VotesController
 {
+    /**
+     * This function is useful to know if the User had already voted for this comment
+     * @param int $idComment it's the id of the comment which is voted by the user
+     * @return bool if the user has already voted for this comment or not
+     */
     public static function hasVoted($idComment){
         if(isset($_SESSION['userid'])){
             $votes = Votes::fetchSomething($idComment,"comments");
@@ -11,25 +19,43 @@ class VotesController
         }
     }
 
+    /**
+     * This function add a New votes to the database
+     * @param int $value this can be 1 or -1 and corresponds to a downvote or an upvote
+     * @param int $idcomments this is the id of the comment which is voted by the user
+     * @return void the controller ask to the model to save in db the changes via save()
+     */
     public static function parseAdd($value,$idComments){
-            $vote = new Votes;
-            $vote->setAuthor($_SESSION['userid']);
-            $vote->setComments($idComments);
-            $vote->setValue($value);
-            $vote->save();
+        $vote = new Votes;
+        $vote->setAuthor($_SESSION['userid']);
+        $vote->setComments($idComments);
+        $vote->setValue($value);
+        $vote->save();
     }
 
+    /**
+     * This function delete a comment 
+     * @param int $id the id of the vote we want to delete in the table
+     * @return void
+     * @throws Exception if the vote doesn't exist
+     */
     public static function parseDelete($id){
         $vote=Votes::fetchSomething($id,"id");
         if(!$vote::delete($id))
-            throw new Exception("Vote don't exist", 1);
+            throw new Exception("Vote doesn't exist", 1);
     }
 
+    /**
+     * This function update a vote, if the value is now 0 after the new vote, 
+     * we just delete the row in db, otherwise, we update the vote with the entry table.
+     * all entry elements are set, it had been verified in CodeController
+     * @param array $entry all the entry to update the Vote table
+     * @return void
+     */
     public static function parseUpdate($entry){
         $vote = Votes::fetchSomething($entry['id'],"id");
         if($vote->getValue()+$entry['value']===0)
-            if(!$vote::delete($entry['id']))
-                throw new Exception("Vote don't exist", 1);
+            VotesController::parseDelete($entry['id']);
         else
             Votes::update($entry);
     }
