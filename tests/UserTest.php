2dcc61f1f299096c8434005b08d9444bab4a20e2
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-27 16:30:05 +0200

    create 2 tests for the user class

---
+++ tests/UserTest.php
@@ -0,0 +1,24 @@
+<?php 
+use PHPUnit\Framework\TestCase;
+
+//to test : ./vendor/bin/phpunit --testdox tests
+//in bash in the folder Awb-g4-Parcode
+
+final class UserTest extends TestCase
+{
+    public function testParamUser(){
+        $this->assertEquals(User::getParam(),[
+			"name" => PDO::PARAM_STR,
+			"pass" => PDO::PARAM_STR,
+			"id" => PDO::PARAM_INT
+		]);
+    }
+    
+    public function testNewUser(){
+        $user = new User;
+        $user->setName("User");
+        $user->setPass(password_hash("user",PASSWORD_DEFAULT));
+        $this->assertEquals("User",$user->getName());
+        $this->assertTrue(password_verify("user",$user->getPass()));   
+    }
+}
